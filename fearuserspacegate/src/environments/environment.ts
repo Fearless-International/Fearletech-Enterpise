5a1c0d85599b6a09b1dfe1f79837df618bef0d08
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-08-09 17:56:56 +0000

    Delete fearuserspacegate directory

--- fearuserspacegate/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:1337/api', // Update with your Strapi backend URL
-  angularDashboardUrl: 'http://localhost:4200/dashboard'
-};
