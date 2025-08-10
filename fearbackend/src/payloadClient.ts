d4dfe6408f0dd13da2f4e0f02e1d2590a112c3ae
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-08-10 02:46:11 +0000

    Update payloadClient.ts

--- fearbackend/src/payloadClient.ts
+++ fearbackend/src/payloadClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log('Axios:', axios);
 
 const payloadClient = axios.create({
-    baseURL: 'http://localhost:3000/api', // Correct API endpoint
+    baseURL: 'https://content.fearlessint.com/api', // Correct API endpoint
 });
 
 export default payloadClient;
