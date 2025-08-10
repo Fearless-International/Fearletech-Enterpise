c9e0f785f40dc373a47a1137430632a363d29806
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-08-10 10:02:52 +0000

    Delete fearbackend directory

--- fearbackend/src/payloadClient.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-console.log('Axios:', axios);
-
-const payloadClient = axios.create({
-    baseURL: 'https://content.fearlessint.com:3000/api', // Correct API endpoint
-});
-
-export default payloadClient;
