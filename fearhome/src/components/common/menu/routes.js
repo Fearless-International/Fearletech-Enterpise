57ebeb6deee7d5bbd66aa823c822cff23fe8e394
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-08-20 14:32:47 +0000

    Fix axios imports and update API calls

--- fearhome/src/components/common/menu/routes.js
+++ fearhome/src/components/common/menu/routes.js
@@ -9,27 +9,27 @@ export const routes = [
 		subRoutes: [
 			{
 				title: "WDD",
-				href: "single-service",
+				href: "/web-services",
 			},
 			{
 				title: "Creative Branding",
-				href: "/",
+				href: "/creative-branding",
 			},
 			{
 				title: "UI/UX Interfaces",
-				href: "/",
+				href: "/uiux-interfaces",
 			},
 			{
 				title: "MAD",
-				href: "/",
+				href: "/mobile-app-development",
 			},
 			{
 				title: "VEA",
-				href: "/",
+				href: "/video-editing-and-animation",
 			},
 			{
 				title: "Software Development",
-				href: "/",
+				href: "/software-development",
 			},
 			{
 				title: "Many more...",
