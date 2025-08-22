654db2780ff5d534e0300c764d00154637f0cc31
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-08-22 22:54:18 +0000

    Fix axios imports and update API calls

--- fearhome/src/components/about/story/StoryPhotos.jsx
+++ fearhome/src/components/about/story/StoryPhotos.jsx
@@ -1,7 +1,7 @@
-import Story1Img from "../../../assets/images/about/image5.png";
-import Story2Img from "../../../assets/images/about/story2.png";
-import Story3Img from "../../../assets/images/about/story3.png";
-import Story4Img from "../../../assets/images/about/story4.png";
+import Story1Img from "../../../assets/images/about/pexels-eva-bronzini-7661184.jpg";
+import Story2Img from "../../../assets/images/about/ZOUZA_D82.png";
+import Story3Img from "../../../assets/images/about/pexels-harold-vasquez-853421-2653362.jpg";
+import Story4Img from "../../../assets/images/about/pexels-makrufinmuhammad-33538425.jpg";
 function StoryPhotos() {
 	return (
 		<>
