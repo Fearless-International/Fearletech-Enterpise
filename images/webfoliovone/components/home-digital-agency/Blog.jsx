2447ad14d408b502d9dab83be225c97662c80c43
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-09-14 12:27:48 +0000

    Success

--- images/webfoliovone/components/home-digital-agency/Blog.jsx
+++ images/webfoliovone/components/home-digital-agency/Blog.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { payloadClient } from '../lib/payloadClient';
 
 function Blog() {
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  }, []);
+
+  const fetchBlogPosts = async () => {
+    setLoading(true);
+    try {
+      const response = await payloadClient.getBlogPosts(1, 3);
+      setBlogPosts(response.docs);
+    } catch (error) {
+      console.error('Error fetching blog posts:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    });
+  };
+
   return (
     <section className="blog style2">
       <div className="container">
@@ -25,95 +54,49 @@ function Blog() {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-4">
-            <div className="item md-mb50">
-              <div className="info sub-title p-color d-flex align-items-center mb-20">
-                <div>
-                  <a href="/blog-grid-sidebar">By : Admin</a>
-                </div>
-                <div className="ml-30">
-                  <a href="/blog-grid-sidebar">August 14, 2023</a>
-                </div>
-              </div>
-              <div className="img fit-img">
-                <img src="/assets/imgs/blog/1.jpg" alt="" />
-              </div>
-              <div className="cont pt-30">
-                <h5>Design Inspiration: Where to Find Creative Ideas</h5>
-                <a
-                  href="/blog-details"
-                  className="butn-crev d-flex align-items-center mt-30"
-                >
-                  <span className="hover-this">
-                    <span className="circle hover-anim">
-                      <i className="ti-arrow-top-right"></i>
-                    </span>
-                  </span>
-                  <span className="text">Read more</span>
-                </a>
-              </div>
-            </div>
+
+        {loading ? (
+          <div className="loader">
+            <div className="spinner"></div>
           </div>
-          <div className="col-lg-4">
-            <div className="item md-mb50">
-              <div className="info sub-title p-color d-flex align-items-center mb-20">
-                <div>
-                  <a href="/blog-grid-sidebar">By : Admin</a>
+        ) : (
+          <div className="row">
+            {blogPosts.map((post, index) => (
+              <div key={post.id} className="col-lg-4">
+                <div className={`item ${index < blogPosts.length - 1 ? 'md-mb50' : ''}`}>
+                  <div className="info sub-title p-color d-flex align-items-center mb-20">
+                    <div>
+                      <a href={post.blogGridLink}>By : {post.author}</a>
+                    </div>
+                    <div className="ml-30">
+                      <a href={post.blogGridLink}>{formatDate(post.publishDate)}</a>
+                    </div>
+                  </div>
+                  <div className="img fit-img">
+                    <img 
+                      src={`${process.env.VITE_PAYLOAD_URL}${post.image?.url}`} 
+                      alt={post.image?.alt || post.title} 
+                    />
+                  </div>
+                  <div className="cont pt-30">
+                    <h5>{post.title}</h5>
+                    <a
+                      href={post.blogDetailsLink}
+                      className="butn-crev d-flex align-items-center mt-30"
+                    >
+                      <span className="hover-this">
+                        <span className="circle hover-anim">
+                          <i className="ti-arrow-top-right"></i>
+                        </span>
+                      </span>
+                      <span className="text">Read more</span>
+                    </a>
+                  </div>
                 </div>
-                <div className="ml-30">
-                  <a href="/blog-grid-sidebar">August 14, 2023</a>
-                </div>
               </div>
-              <div className="img fit-img">
-                <img src="/assets/imgs/blog/2.jpg" alt="" />
-              </div>
-              <div className="cont pt-30">
-                <h5>Typography: Choosing Fonts for Maximum Impact</h5>
-                <a
-                  href="/blog-details"
-                  className="butn-crev d-flex align-items-center mt-30"
-                >
-                  <span className="hover-this">
-                    <span className="circle hover-anim">
-                      <i className="ti-arrow-top-right"></i>
-                    </span>
-                  </span>
-                  <span className="text">Read more</span>
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
-          <div className="col-lg-4">
-            <div className="item">
-              <div className="info sub-title p-color d-flex align-items-center mb-20">
-                <div>
-                  <a href="/blog-grid-sidebar">By : Admin</a>
-                </div>
-                <div className="ml-30">
-                  <a href="/blog-grid-sidebar">August 14, 2023</a>
-                </div>
-              </div>
-              <div className="img fit-img">
-                <img src="/assets/imgs/blog/3.jpg" alt="" />
-              </div>
-              <div className="cont pt-30">
-                <h5>Design Inspiration: Where to Find Creative Ideas</h5>
-                <a
-                  href="/blog-details"
-                  className="butn-crev d-flex align-items-center mt-30"
-                >
-                  <span className="hover-this">
-                    <span className="circle hover-anim">
-                      <i className="ti-arrow-top-right"></i>
-                    </span>
-                  </span>
-                  <span className="text">Read more</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </section>
   );
