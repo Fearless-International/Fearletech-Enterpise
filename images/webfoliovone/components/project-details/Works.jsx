5b16fded0893e6af536c285c3d843d632c991926
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-10-01 00:00:01 +0000

    done

--- images/webfoliovone/components/project-details/Works.jsx
+++ images/webfoliovone/components/project-details/Works.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 
-function Works() {
+function Works({ project }) {
+  const images = project?.projectImages || [];
+  
   return (
     <div className="img-column">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6">
-            <div className="img md-mb30">
-              <img src="/assets/imgs/works/3/2.jpg" alt="" />
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="img">
-              <img src="/assets/imgs/works/3/3.jpg" alt="" />
-            </div>
-          </div>
+          {images.length >= 2 ? (
+            <>
+              <div className="col-lg-6">
+                <div className="img md-mb30">
+                  <img src={images[0].imageUrl} alt={images[0].caption || ''} />
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="img">
+                  <img src={images[1].imageUrl} alt={images[1].caption || ''} />
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="col-lg-6">
+                <div className="img md-mb30">
+                  <img src="/assets/imgs/works/3/2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="img">
+                  <img src="/assets/imgs/works/3/3.jpg" alt="" />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
--- images/webfoliovone/components/project-details/Wroks2.jsx
+++ images/webfoliovone/components/project-details/Wroks2.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function Wroks2() {
+function Wroks2({ project }) {
+  const images = project?.projectImages || [];
+  
   return (
-    <div className="section-padding pt-0">
+    <div className="img-column">
       <div className="container">
-        <div className="img">
-          <img src="/assets/imgs/works/3/6.jpg" alt="" />
+        <div className="row">
+          {images.length >= 4 ? (
+            <>
+              <div className="col-lg-6">
+                <div className="img md-mb30">
+                  <img src={images[2].imageUrl} alt={images[2].caption || ''} />
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="img">
+                  <img src={images[3].imageUrl} alt={images[3].caption || ''} />
+                </div>
+              </div>
+            </>
+          ) : null}
         </div>
       </div>
     </div>
