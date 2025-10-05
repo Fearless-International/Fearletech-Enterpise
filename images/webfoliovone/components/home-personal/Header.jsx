b91e67108d8e34019548b8637c30c33ed90f6fc5
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-10-05 23:09:06 +0000

    done

--- images/webfoliovone/components/home-personal/Header.jsx
+++ images/webfoliovone/components/home-personal/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
   return (
     <div
       className="header header-personal valign bg-img"
-      data-background="/assets/imgs/header/p00.png"
+      data-background="/assets/imgs/header/p001.png"
       data-overlay-dark="2"
     >
       <div className="container ontop">
