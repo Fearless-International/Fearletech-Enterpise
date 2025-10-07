b6d6f99bff83eed65e28df485aaad107674dc09a
Marius - Junior FRANCISCO <124208478+fmjmadeit@users.noreply.github.com>
2025-10-07 16:17:28 +0000

    done

--- images/webfoliovone/components/page-contact/Map.jsx
+++ images/webfoliovone/components/page-contact/Map.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 function Map() {
   return (
     <div className="google-map">
-      <iframe
-        id="gmap_canvas"
-        src="https://maps.app.goo.gl/wPVwSvEZ9hzTowha7"
-      ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127066.72272946908!2d-0.2621329264874027!3d5.591373811105403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9084b2b7a773%3A0xbed14ed8650e2dd3!2sAccra!5e0!3m2!1sen!2sgh!4v1759853785642!5m2!1sen!2sgh" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
